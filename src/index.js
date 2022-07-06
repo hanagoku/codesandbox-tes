330c261944f6abaddcd9063923e5fa94781fb9b3
hanagoku <108511558+hanagoku@users.noreply.github.com>
2022-07-06 14:43:22 +0900

    完了まで 06071443

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import "./styles.css";
-console.log("あああああああ");
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-//テキストボックスの値を取得し初期化する
 const onclickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
